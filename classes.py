0ebb8decef4eac7b65181039cc29614a70ce3375
kartash <kartashovAlex2008@yandex.ru>
2025-01-14 18:39:17 +0300

    BaseObject.draw()

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import pygame
+
 from data_types import Coord, Rect
 
 
@@ -23,7 +25,18 @@ class BaseObject:
         pass
 
     def draw(self, screen):
-        pass
+        sprites = pygame.sprite.Group()
+        sprite = pygame.sprite.Sprite()
+
+        sprite.image = pygame.image.load(self.image_file)
+        sprite.rect = sprite.image.get_rect()
+        sprite.size = sprite.image.get_size()
+
+        sprite.rect.x = int(self.coords[0] - sprite.size[0] / 2)
+        sprite.rect.y = int(self.coords[1] - sprite.size[1] / 2)
+
+        sprites.add(sprite)
+        sprites.draw(screen)
 
 
 class BaseCharacter(BaseObject):
